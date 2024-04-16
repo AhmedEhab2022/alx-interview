7e486c72e4d62bd506a62f656d561cce19470cb0
AhmedEhab2022 <am8122004@gmail.com>
2024-04-16 18:11:30 +0200

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+  """ determines if all the boxes can be opened """
+
+  n = len(boxes)
+  openedKeys = set(boxes[0])
+  openedKeys.add(0)
+  for j in range(2):
+    for i in range(1, n):
+      if i in openedKeys:
+        openedKeys.update(boxes[i])
+
+  if len(openedKeys) >= n:
+    return True
+  else:
+    return False
+    
