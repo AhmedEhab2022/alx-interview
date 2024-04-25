155fbab7a8f8e7141b12fa46706bf4c9772c555b
AhmedEhab2022 <am8122004@gmail.com>
2024-04-25 22:50:07 +0200

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module contains the minOperations function
+"""
+
+
+def minOperations(n):
+    """
+    Method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+
+    Args:
+        n (int): The number of characters to be printed.
+
+    Returns:
+        int: The fewest number of operations needed to
+        result in exactly n H characters in the file
+        or 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+    elif n == 2:
+        return 2
+
+    ans = minOperationsRec(n, 2, 1, 2)
+    if ans == float('inf'):
+        return 0
+
+    return ans
+
+
+def minOperationsRec(n, Hnum, HprevNum, opCount):
+    """
+    Recursive function to calculate the fewest number of operations needed.
+    """
+    if Hnum == n:
+        return opCount
+    elif Hnum > n:
+        return float('inf')
+
+    return min(minOperationsRec(n, Hnum + HprevNum, HprevNum, opCount + 1),
+               minOperationsRec(n, Hnum * 2, Hnum, opCount + 2))
