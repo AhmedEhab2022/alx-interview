cafdc27dc0646afacc15ef762c2fdec11b286f5a
AhmedEhab2022 <am8122004@gmail.com>
2024-05-21 13:05:06 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,32 @@
 #!/usr/bin/node
+
+const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/';
 
 async function getCharactersOfMovie (movieId) {
-  const response = await fetch(`${url}films/${movieId}`);
-  const data = await response.json();
-  return data.characters;
+  return await new Promise((resolve, reject) => {
+    request(`${url}films/${movieId}`, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body).characters);
+    });
+  });
 }
 
 async function DisplayStarWarsCharactersOfMovie (movieId) {
   const charactersArray = await getCharactersOfMovie(movieId);
   for (const characterUrl of charactersArray) {
-    const response = await fetch(characterUrl);
-    const data = await response.json();
-    console.log(data.name);
+    await new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        console.log(JSON.parse(body).name);
+        resolve(JSON.parse(body).name);
+      });
+    });
   }
 }
 
