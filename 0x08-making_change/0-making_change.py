0118fc8d6a6f6b8535d63b67df428c1b4034fe76
AhmedEhab2022 <am8122004@gmail.com>
2024-06-20 16:06:33 +0300

    fix style

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """ Making Change """
 
+
 def makeChange(coins, total):
-	""" Given a pile of coins of different values, determine the fewest
-		number of coins needed to meet a given amount total.
-	"""
-	if total <= 0:
-		return 0
+    """ Given a pile of coins of different values, determine the fewest
+        number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
 
-	n = len(coins)
-	dp = [0]
-	for _ in range(total):
-		dp.append(total + 1)
+    dp = [total + 1 for k in range(total + 1)]
+    dp[0] = 0
+    for i in range(1, total + 1):
+        for j in coins:
+            if j <= i:
+                if dp[i - j] == total + 1 or dp[i - j] == -1:
+                    dp[i] = -1
+                    continue
+                dp[i] = min(dp[i - j] + 1, dp[i])
 
-	for i in range(1, total + 1):
-		for j in coins:
-			if j <= i:
-				if dp[i - j] == total + 1 or dp[i - j] == -1:
-					dp[i] = -1
-					continue
-				dp[i] = min(dp[i - j] + 1, dp[i])
-
-	return dp[total]
+    return dp[total]
