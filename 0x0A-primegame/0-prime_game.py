46e557ba1eb1e1561887ab728eceeb2d0db0fb53
AhmedEhab2022 <am8122004@gmail.com>
2024-07-03 14:21:12 +0300

    0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,7 +13,7 @@ def sieveOfEratosthenes(n):
     prime = [True for i in range(n + 1)]
     p = 2
     while (p * p <= n):
-        if (prime[p] == True):
+        if (prime[p] is True):
             for i in range(p * p, n + 1, p):
                 prime[i] = False
         p += 1
